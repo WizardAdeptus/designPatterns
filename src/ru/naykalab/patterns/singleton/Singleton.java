f747a975990786ee088e010240a8956ca5e230f2
Igor <wizard.adeptus@ya.ru>
2017-05-25 19:03:37 +0400

    singleton

---
+++ src/ru/naykalab/patterns/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package ru.naykalab.patterns.singleton;
+
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+    
+    private Singleton(){}
+    
+    public static Singleton getInstance(){
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
---
+++ src/ru/naykalab/patterns/singleton/SingletonSync.java
@@ -0,0 +1,16 @@
+package ru.naykalab.patterns.singleton;
+
+
+public class SingletonSync {
+    private static SingletonSync instSingleton;
+
+    private SingletonSync() {
+    }
+    
+    public static synchronized SingletonSync getInstance() {
+        if (instSingleton == null) {
+            instSingleton = new SingletonSync();
+        }
+        return instSingleton;
+    }
+}
